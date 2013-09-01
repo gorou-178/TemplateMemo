77ade24c7d674dbb7f5ba565444926482581c108
gurimmer <s.reianai@gmail.com>
2013-09-01 16:55:38 +0900

    Multilingual

--- TemplateMemoApp/FontDataSource.m
+++ TemplateMemoApp/FontDataSource.m
@@ -12,6 +12,7 @@
 #import "FontSize.h"
 #import "FontSizeSettingInfo.h"
 #import "UserDefaultsWrapper.h"
+#import "UIDeviceHelper.h"
 
 @implementation FontDataSource
 
@@ -30,45 +31,76 @@
     systemFont.uiFont = [UIFont systemFontOfSize:fontSize.size];
     systemFont.row = 0;
     systemFont.name = [systemFont.uiFont fontName];
-    systemFont.labelText = [systemFont.uiFont fontName];
+    systemFont.labelText = [systemFont.uiFont familyName];
     [self.dataList addObject:systemFont];
-
-    // ヒラギノ角ゴ ProN W3
-    Font *hiraginoKakuGothic = [[Font alloc] init];
-    hiraginoKakuGothic.uiFont = [UIFont fontWithName:@"HiraKakuProN-W3" size:fontSize.size];
-    hiraginoKakuGothic.row = 1;
-    hiraginoKakuGothic.name = [hiraginoKakuGothic.uiFont fontName];
-    hiraginoKakuGothic.labelText = [hiraginoKakuGothic.uiFont fontName];
-    [self.dataList addObject:hiraginoKakuGothic];
     
-    // Helvetica NeueUI
-    Font *helvetica = [[Font alloc] init];
-    helvetica.uiFont = [UIFont fontWithName:@".HelveticaNeueUI" size:fontSize.size];
-    helvetica.row = 2;
-    helvetica.name = [helvetica.uiFont fontName];
-    helvetica.labelText = [helvetica.uiFont fontName];
-    [self.dataList addObject:helvetica];
-    
-    Font *bokutachinoGothic = [[Font alloc] init];
-    bokutachinoGothic.uiFont = [UIFont fontWithName:@"BokutachinoGothic" size:fontSize.size];
-    bokutachinoGothic.row = 3;
-    bokutachinoGothic.name = [bokutachinoGothic.uiFont fontName];
-    bokutachinoGothic.labelText = [bokutachinoGothic.uiFont fontName];
-    [self.dataList addObject:bokutachinoGothic];
-    
-    Font *hannariMincho = [[Font alloc] init];
-    hannariMincho.uiFont = [UIFont fontWithName:@"HannariMincho" size:fontSize.size];
-    hannariMincho.row = 4;
-    hannariMincho.name = [hannariMincho.uiFont fontName];
-    hannariMincho.labelText = [hannariMincho.uiFont fontName];
-    [self.dataList addObject:hannariMincho];
-    
-    Font *huiFontP = [[Font alloc] init];
-    huiFontP.uiFont = [UIFont fontWithName:@"HuiFontP" size:fontSize.size];
-    huiFontP.row = 5;
-    huiFontP.name = [huiFontP.uiFont fontName];
-    huiFontP.labelText = [huiFontP.uiFont fontName];
-    [self.dataList addObject:huiFontP];
+    if ([UIDeviceHelper isJapaneseLanguage]) {
+        // ヒラギノ角ゴ ProN W3
+        Font *hiraginoKakuGothic = [[Font alloc] init];
+        hiraginoKakuGothic.uiFont = [UIFont fontWithName:@"HiraKakuProN-W3" size:fontSize.size];
+        hiraginoKakuGothic.row = 1;
+        hiraginoKakuGothic.name = [hiraginoKakuGothic.uiFont fontName];
+        //    hiraginoKakuGothic.labelText = [hiraginoKakuGothic.uiFont familyName];
+        hiraginoKakuGothic.labelText = @"ヒラギノ角ゴ ProN W3";
+        [self.dataList addObject:hiraginoKakuGothic];
+        
+        Font *bokutachinoGothic = [[Font alloc] init];
+        bokutachinoGothic.uiFont = [UIFont fontWithName:@"BokutachinoGothic" size:fontSize.size];
+        bokutachinoGothic.row = 2;
+        bokutachinoGothic.name = [bokutachinoGothic.uiFont fontName];
+        //    bokutachinoGothic.labelText = [bokutachinoGothic.uiFont familyName];
+        bokutachinoGothic.labelText = @"ぼくたちのゴシック";
+        [self.dataList addObject:bokutachinoGothic];
+        
+        Font *hannariMincho = [[Font alloc] init];
+        hannariMincho.uiFont = [UIFont fontWithName:@"HannariMincho" size:fontSize.size];
+        hannariMincho.row = 3;
+        hannariMincho.name = [hannariMincho.uiFont fontName];
+        //    hannariMincho.labelText = [hannariMincho.uiFont familyName];
+        hannariMincho.labelText = @"はんなり明朝";
+        [self.dataList addObject:hannariMincho];
+        
+        Font *huiFontP = [[Font alloc] init];
+        huiFontP.uiFont = [UIFont fontWithName:@"HuiFontP" size:fontSize.size];
+        huiFontP.row = 4;
+        huiFontP.name = [huiFontP.uiFont fontName];
+        //    huiFontP.labelText = [huiFontP.uiFont familyName];
+        huiFontP.labelText = @"ふい字Ｐ";
+        [self.dataList addObject:huiFontP];
+    }
+    else {
+        // Times New Roman
+        Font *timesNewRoma = [[Font alloc] init];
+        timesNewRoma.uiFont = [UIFont fontWithName:@"TimesNewRomanPSMT" size:fontSize.size];
+        timesNewRoma.row = 1;
+        timesNewRoma.name = [timesNewRoma.uiFont fontName];
+        timesNewRoma.labelText = [timesNewRoma.uiFont familyName];
+        [self.dataList addObject:timesNewRoma];
+        
+        // Gill Sans
+        Font *gillSans = [[Font alloc] init];
+        gillSans.uiFont = [UIFont fontWithName:@"GillSans" size:fontSize.size];
+        gillSans.row = 2;
+        gillSans.name = [gillSans.uiFont fontName];
+        gillSans.labelText = [gillSans.uiFont familyName];
+        [self.dataList addObject:gillSans];
+        
+        // Chalkboard SE Light
+        Font *chalkboard = [[Font alloc] init];
+        chalkboard.uiFont = [UIFont fontWithName:@"ChalkboardSE-Light" size:fontSize.size];
+        chalkboard.row = 3;
+        chalkboard.name = [chalkboard.uiFont fontName];
+        chalkboard.labelText = [chalkboard.uiFont familyName];
+        [self.dataList addObject:chalkboard];
+        
+        // Avenir-Book
+        Font *avenir = [[Font alloc] init];
+        avenir.uiFont = [UIFont fontWithName:@"Avenir-Book" size:fontSize.size];
+        avenir.row = 4;
+        avenir.name = [avenir.uiFont fontName];
+        avenir.labelText = [avenir.uiFont familyName];
+        [self.dataList addObject:avenir];
+    }
 
     return self;
 }
--- TemplateMemoApp/FontSizeDataSource.m
+++ TemplateMemoApp/FontSizeDataSource.m
@@ -25,32 +25,32 @@
     
     FontSize *fontSize = [[FontSize alloc] init];
     fontSize.row = 0;
-    fontSize.size = 14;
-    fontSize.labelText = [[NSString alloc] initWithFormat:@"最小(%gpt)", fontSize.size];
+    fontSize.size = 12;
+    fontSize.labelText = [[NSString alloc] initWithFormat:NSLocalizedString(@"setting.fontsize.verysmall", @"setting fontsize - very small"), fontSize.size];
     [self.dataList addObject:fontSize];
     
     fontSize = [[FontSize alloc] init];
     fontSize.row = 1;
-    fontSize.size = 18;
-    fontSize.labelText = [[NSString alloc] initWithFormat:@"小(%gpt)", fontSize.size];
+    fontSize.size = 14;
+    fontSize.labelText = [[NSString alloc] initWithFormat:NSLocalizedString(@"setting.fontsize.small", @"setting fontsize - small"), fontSize.size];
     [self.dataList addObject:fontSize];
     
     fontSize = [[FontSize alloc] init];
     fontSize.row = 2;
-    fontSize.size = 24;
-    fontSize.labelText = [[NSString alloc] initWithFormat:@"中(%gpt)", fontSize.size];
+    fontSize.size = 18;
+    fontSize.labelText = [[NSString alloc] initWithFormat:NSLocalizedString(@"setting.fontsize.normal", @"setting fontsize - normal"), fontSize.size];
     [self.dataList addObject:fontSize];
     
     fontSize = [[FontSize alloc] init];
     fontSize.row = 3;
-    fontSize.size = 36;
-    fontSize.labelText = [[NSString alloc] initWithFormat:@"大(%gpt)", fontSize.size];
+    fontSize.size = 24;
+    fontSize.labelText = [[NSString alloc] initWithFormat:NSLocalizedString(@"setting.fontsize.large", @"setting fontsize - large"), fontSize.size];
     [self.dataList addObject:fontSize];
     
     fontSize = [[FontSize alloc] init];
     fontSize.row = 4;
-    fontSize.size = 48;
-    fontSize.labelText = [[NSString alloc] initWithFormat:@"最大(%gpt)", fontSize.size];
+    fontSize.size = 36;
+    fontSize.labelText = [[NSString alloc] initWithFormat:NSLocalizedString(@"setting.fontsize.extralarge", @"setting fontsize - extra large"), fontSize.size];
     [self.dataList addObject:fontSize];
     
     return self;
